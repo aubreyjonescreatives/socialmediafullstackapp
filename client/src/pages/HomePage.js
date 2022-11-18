22a39503cdea2e045b107b52b9c8839c54564dd0
Aubrey Jones <aubrey_jones07@hotmail.com>
2022-11-18 03:54:18 -0700

    started to hard code post info on homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,82 @@
 import React from 'react' 
-
+import { Link } from 'react-router-dom'
 
 
 const HomePage = () => {
 
-return (
 
-<div>HomePage</div>
+const posts = [
+{
+id: 1, 
+title: 'Lorem ipsum dolor sit amet consectetur', 
+desc: 'Lorem ipsum dolor sit amet consectetur', 
+img: '',
 
 
+}, 
+{
+    id: 2, 
+    title: 'Lorem ipsum dolor sit amet consectetur', 
+    desc: 'Lorem ipsum dolor sit amet consectetur', 
+    img: '',
+    
+    
+    }, 
+    {
+        id: 3, 
+        title: 'Lorem ipsum dolor sit amet consectetur', 
+        desc: 'Lorem ipsum dolor sit amet consectetur', 
+        img: '',
+        
+        
+        }, 
+        {
+            id: 4, 
+            title: 'Lorem ipsum dolor sit amet consectetur', 
+            desc: 'Lorem ipsum dolor sit amet consectetur', 
+            img: '',
+            
+            
+            }, 
+            {
+                id: 5, 
+                title: 'Lorem ipsum dolor sit amet consectetur', 
+                desc: 'Lorem ipsum dolor sit amet consectetur', 
+                img: '',
+                
+                
+                }, 
+
+
+
+
+
+
+]
+
+
+
+
+    return (
+
+<div className='home'>
+<div className="posts">
+{posts.map(post=>(
+<div className="post" key={post.id}>
+    <div className="img">
+    <img src={post.img} alt=""/>
+    </div>
+    <div className="content">
+        <Link to={`/post/${post.id}`}>
+        <h1>{post.title}</h1>
+        <p>{post.desk}</p>
+        </Link>
+    </div>
+</div>
+))}
+
+</div>
+</div>
 )
 
 
