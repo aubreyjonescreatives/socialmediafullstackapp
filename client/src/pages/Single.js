c47a012c82379f79364682f415ffc80ef659fedc
Aubrey Jones <aubrey_jones07@hotmail.com>
2022-11-18 04:13:35 -0700

    started to add single post organization

--- client/src/pages/Single.js
+++ client/src/pages/Single.js
@@ -5,9 +5,22 @@ import React from 'react'
 const Single = () => {
 
 return (
+<div className='single'>
+<div className="content"> 
+    <img src=''/>
+    <div className="user">
+    <img src=""/>
+    <div className="info">
+    <span>John</span>
+    <p>Posted 2 days ago</p>
+    </div>
+    </div>
+</div>
+<div className="menu">m
+</div>
 
-<div>Single</div>
 
+</div>
 
 )
 
