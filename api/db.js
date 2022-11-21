3bdba55ee4c87052fddce7a63f532b8aa7332891
Aubrey Jones <aubrey_jones07@hotmail.com>
2022-11-20 20:42:23 -0700

    added sql creds and updated .env file for security

--- api/db.js
+++ api/db.js
@@ -1,9 +1,2 @@
 import mysql from 'mysql'
 
-
-export const db = mysql.createConnection({
-   host:'localhost', 
-   user:'root',  
-   password: '',
-   database: '',  
-})
--- api/index.js
+++ api/index.js
@@ -1,11 +1,40 @@
 import express from 'express' 
+import mysql from 'mysql'
+import * as dotenv from 'dotenv'
 
+
+const port = process.env.SERVER_PORT || 5500 
 
 const app = express()
 
-app.use(express.json())
+dotenv.config()
+
+const db = mysql.createConnection({
+    host:'localhost', 
+    user:'root', 
+    password: process.env.DB_PASSWORD, 
+    database:"test" 
+})
+
+// If there is an auth problem 
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Astronomy11!'; 
 
 
-app.listen(5500, ()=> {
-    console.log("Server has started!")
+
+app.get('/', (req, res) => {
+    res.json("Hello this is the backend")
+})
+
+app.get("/posts", (req, res) => {
+    const q = "SELECT * FROM posts"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Server has started on port ${port}`)
 })
